507141966f7674bc0c553311da7d97b75cedecc9
Vector Sigma <anishchy501625@gmail.com>
2024-08-02 18:51:18 -0700

    working on profile section

--- frontend/jobportal/src/App.jsx
+++ frontend/jobportal/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import './index.css'
+import Header from './components/common/Header';
+import UserProfile from './components/profile/UserProfile';
 
 function App() {
-  
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />}/>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/userprofile' element={<UserProfile />} />
       </Routes>
     </Router>
   )
