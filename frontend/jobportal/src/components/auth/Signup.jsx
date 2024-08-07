9d232d2a22a178c9d418648c3d356ec4a1580064
Vector Sigma <anishchy501625@gmail.com>
2024-08-07 11:52:23 -0700

    started working with authentication

--- frontend/jobportal/src/components/auth/Signup.jsx
+++ frontend/jobportal/src/components/auth/Signup.jsx
@@ -1,85 +1,125 @@
-import { Link } from 'react-router-dom'; 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { Checkbox, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const Signup = () => {
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
-    const [passwordOutput, setPasswordOutput] = useState(true);
-
-    const handlepass1 = (event) => {
-        setPassword1(event.target.value);
-    }
-    const handlepass2 = (event) => {
-        setPassword2(event.target.value);
-    }
-    
-    const handleSubmit = (event) => {
-        event.preventDefault(); 
-        if (password1 !== password2) {
-          setPasswordOutput(false);
-        } else {
-          setPasswordOutput(true);
-        }
-      };
-      
+  const [email,setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [conPassword, setConPassword] = useState('');
+  const [tc,setTc] = useState(false);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-          Sajha Kaam     
-        </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-              </div>
-              <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" value={password1} onChange={handlepass1} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-              </div>
-              <div>
-                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                <input type="password" name="confirm-password" id="confirm-password" value={password2} onChange={handlepass2} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-                    I accept the <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Terms and Conditions</a>
-                  </label>
-                </div>
-              </div>
-              <button type="submit" onClick={handleSubmit} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                Create an account
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{' '}
-                <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                  Login here
-                </Link>
-              </p>
-            </form>
-            {passwordOutput ? null : <PasswordIncorrect /> }
-          </div>
-        </div>
-      </div>
-    </section>
+    <Container
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '68vh',
+    }}
+    >
+    <Box
+    sx={{
+      backgroundColor: 'whitesmoke',
+      p: '80px',
+    }}
+    >
+      <form
+      >
+      <Typography
+      variant='h5'
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        mb: '30px',
+      }}
+      ><b>Signup</b>
+      </Typography>
+      <Box>
+        <Typography>
+          Email
+        </Typography>
+        <TextField 
+        sx={{
+          mb: '10px'
+        }}
+        size='small'
+        label="Email" value={email} variant="outlined" margin="normal" onChange={(e) => setEmail(e.target.value)} />
+      </Box>
+      <Box>
+        <Typography>
+          Full Name
+        </Typography>
+        <TextField 
+        sx={{
+          mb: '10px'
+        }}
+        size='small'
+        label="Full Name" value={name} variant="outlined" margin="normal" onChange={(e) => setName(e.target.value)} />
+      </Box>
+      <Box>
+        <Typography>
+          Password
+        </Typography>
+        <TextField 
+        size="small"
+        sx={{
+          mb: '10px'
+        }}
+        label="Password" value={password} variant="outlined" margin="normal" type="password" onChange={(e) => setPassword(e.target.value)}/>
+      </Box>
+      <Box>
+        <Typography>
+          Confirm Password
+        </Typography>
+        <TextField 
+        size="small"
+        sx={{
+          mb: '10px'
+        }}
+        label="Confirm Password" value={conPassword} variant="outlined" margin="normal" type="password" onChange={(e) => setConPassword(e.target.value)}/>
+      </Box>
+      <FormGroup>
+        <FormControlLabel control={ <Checkbox defaultChecked/>} label="Accept Terms and Conditions" required value={tc} onChange={(e) => setTc(e.target.value)}/>
+      </FormGroup>
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+      <Button
+      sx={{
+        backgroundColor: 'dodgerblue',
+        color: "white",
+        width: "18vw",
+        height: "6vh",
+        "&:hover": {
+            bgcolor: "green",
+          },
+      }}
+      >
+      Submit</Button>
+      </Box>
+
+      <Typography 
+      sx={{
+        fontSize: '12px',
+        mt: '20px'
+      }}
+      >Don't have an account? Sign up</Typography>
+      </form>
+      </Box>
+      {/*if you want to add something at the side you can give a reference */}
+    </Container>
   );
 };
 
 export default Signup;
-
-
-const PasswordIncorrect = () => {
-    return <h1>Please enter matching passwords</h1>;
-  };
-  
